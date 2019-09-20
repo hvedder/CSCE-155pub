14aafc6df1c204cb5da07294fb4fa288a8d45311
hvedder <55569584+hvedder@users.noreply.github.com>
2019-09-19 22:19:34 -0500

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,12 @@
+/* This program was created by Hunter Vedder and Modified by _________
+ * Starting on 9/19/19
+ * Ending on 9/20/19
+ * For the purpose of testing our abbility to use GitHub
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Hello Hunter.\n");
+}
